0b4d91ab416b3bfc0a38d4a30e01205d3d8a1418
Pema Geyleg <pema.geyleg@gmail.com>
2017-04-03 13:50:53 -0700

    Refactors version spec

--- spec/lib/dockerfile/version_spec.rb
+++ spec/lib/dockerfile/version_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe 'version' do
   it 'needs to be documented on CHANGELOG' do
-    gem_spec = Gem::Specification.find_by_name('dockerfile')
-    gem_root = gem_spec.gem_dir
+    gem_root = Gem::Specification.find_by_name('dockerfile').gem_dir
     changelog_file = gem_root + '/CHANGELOG.md'
-    expect(system('grep', "#{Dockerfile::VERSION}", "#{changelog_file}")).to be_truthy
+    version = Dockerfile::VERSION
+    expect(system('grep', version, changelog_file)).to be_truthy
   end
 end
