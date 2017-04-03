a52e45849baecbd4ef184724b4845db3fa11204a
Pema Geyleg <pema.geyleg@gmail.com>
2017-04-03 16:43:16 -0700

    Introduces Dockerfile::Configuration#file

--- lib/configuration.rb
+++ lib/configuration.rb
@@ -1,12 +1,15 @@
 module Dockerfile
   class Configuration
-    FILE_NAME = 'dockerfile.yaml'
-    def config
-      current_dir = "#{Dir.pwd}/#{FILE_NAME}"
-      home_dir = "#{Dir.home}/#{FILE_NAME}"
+    FILE_NAME = 'dockerfile.yaml'.freeze
 
-      file = File.exist?(current_dir) ? current_dir : home_dir
-      { config_file: file }
+    def file
+      env_dir = ENV['DOCKERFILE']
+      if env_dir && File.exist?("#{env_dir}/#{FILE_NAME}")
+        return "#{env_dir}/#{FILE_NAME}"
+      end
+
+      current_dir = "#{Dir.pwd}/#{FILE_NAME}"
+      File.exist?(current_dir) ? current_dir : "#{Dir.home}/#{FILE_NAME}"
     end
   end
 end
--- spec/lib/configuration_spec.rb
+++ spec/lib/configuration_spec.rb
@@ -1,38 +1,42 @@
 require 'spec_helper'
 
 describe Dockerfile::Configuration do
-  context '#config' do
+  context '#file' do
     let(:configuration) { described_class.new }
 
-    it 'looks for config file in current working directory' do
+    it 'returns file from current working directory' do
       expect(Dir).to receive(:pwd).and_return('/work')
-      expect(File).to receive(:exist?).with('/work/dockerfile.yaml').and_return(true)
+      expect(File).to receive(:exist?).with('/work/dockerfile.yaml')
+        .and_return(true)
 
-      expect(configuration.config[:config_file]).to eq('/work/dockerfile.yaml')
+      expect(configuration.file).to eq('/work/dockerfile.yaml')
     end
 
-    it 'looks for config file in home directory if unable to find in current directory' do
+    it 'returns file from home directory if not found in current directory' do
       expect(Dir).to receive(:pwd).and_return('/work')
-      expect(File).to receive(:exist?).with('/work/dockerfile.yaml').and_return(false)
+      expect(File).to receive(:exist?).with('/work/dockerfile.yaml')
+        .and_return(false)
       expect(Dir).to receive(:home).and_return('/home/me')
 
-      expect(configuration.config[:config_file]).to eq('/home/me/dockerfile.yaml')
+      expect(configuration.file).to eq('/home/me/dockerfile.yaml')
     end
 
-    it 'looks for config file specified by environment variable' do
-      expect(ENV).to receive(:DOCKERFILE).and_return('/etc/config')
-      expect(File).to receive(:exist?).with('/etc/config/dockerfile.yaml').and_return(true)
+    it 'looks for file specified by environment variable' do
+      expect(ENV).to receive(:[]).with('DOCKERFILE').at_least(:once)
+        .and_return('/etc/config')
+      expect(File).to receive(:exist?).with('/etc/config/dockerfile.yaml')
+        .and_return(true)
 
       expect(Dir).to_not receive(:pwd)
       expect(Dir).to_not receive(:home)
 
-      expect(configuration.config[:config_file]).to eq('/etc/config/dockerfile.yaml')
+      expect(configuration.file).to eq('/etc/config/dockerfile.yaml')
     end
 
-    it 'raises an error if file specified by environment variable does not exist' do
+    it 'warns user if file specified by environment variable does not exist' do
     end
 
-    it 'raises an error if file does not exist in current directory or home directory' do
+    it 'warns user if file does not exist' do
     end
   end
 end
